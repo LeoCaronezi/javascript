ce0ffc41ed6c175b15e078b8f972f37c8dbc203a
LeoCaronezi <rafael.caronezi@outlook.com>
2019-07-04 16:38:18 -0300

    LeoCaronezi
    utilizando class mathc e string

---
+++ String.js
@@ -0,0 +1,9 @@
+// fomra 1 de trabalha com string 
+var a = "rafael"
+console.log(a)
+// forma 2 de trabalhar com string 
+var b = 'rafael'
+console.log(b)
+//forma 3 de trabalhar com string 
+var c = `rafael`
+console.log(c)
---
+++ math.js
@@ -0,0 +1,4 @@
+const raio =3.38
+const area = Math.PI * Math.pow(raio,5)
+console.log(area)
+console.log(typeof Math)
