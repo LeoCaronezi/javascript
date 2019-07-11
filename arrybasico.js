f879721aced090631eda04deaf451ebdd4838ec4
LeoCaronezi <rafael.caronezi@outlook.com>
2019-07-11 00:15:23 -0300

    LeoCaronezi

---
+++ arrybasico.js
@@ -0,0 +1,5 @@
+// aprendendo o basico de array  em javascript 
+
+// criando array.
+const valores  = [1, 3 , 5]
+console.log(valores[1])
---
+++ objeto.js
@@ -0,0 +1,7 @@
+const any ={}
+any.nome = "rafael"
+any.idade = 26
+any.endereco = "rua ivan serpa numero 10 lote 7 quadra c"
+
+
+console.log(any)
