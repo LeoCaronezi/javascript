ab34d5109b24d3bb46fa07800c094c9f84629913
LeoCaronezi <rafael.caronezi@outlook.com>
2019-07-12 20:19:34 -0300

    LeoCaronezi

---
+++ diferencaentrevarlet.js
@@ -0,0 +1,12 @@
+var  teste = 1 
+console.log(teste)
+{
+    var teste = "seja bem vindo"
+    console.log(teste)
+}
+let  test =1 
+{
+  let  test = 2 
+    console.log(test)
+}
+console.log(test)
---
+++ looppt2.js
@@ -0,0 +1,10 @@
+const fun = []
+// trabalhar com var primeiro 
+for(var i=0; i <10 ; i++){
+    var j = 0
+     j++
+    fun.push(j)
+}
+for (let i =0; i<10; i++){
+    console.log(fun)
+}
