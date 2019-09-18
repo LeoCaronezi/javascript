1a9fbbca8ede002a3f2f0252c0ad433a57acba97
LeoCaronezi <rafael.caronezi@outlook.com>
2019-09-18 15:08:32 -0300

    LeoCaronezi
    curso da udmy

---
+++ cursoUdmy/operacoesmatematicas.js
@@ -0,0 +1,15 @@
+let a = 10 
+let b = 5 
+
+let soma  = a + b
+let sub  = a-b
+let mult  = a *b
+let div = a /b
+
+
+console.log("a variavel a  e :"+ a)
+console.log("a varivel b e :"+ b)
+console.log (soma)
+console.log(sub)
+console.log(mult)
+console.log(div)
